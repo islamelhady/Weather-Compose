d6ee91626c6422529bcd2bd3a19616e24dc726ad
islam elhady <i.elhady44@gmail.com>
2025-06-20 23:48:29 +0300

    Setup Application class to initialize Koin

---
+++ app/src/main/java/com/elhady/weather_compose/di/AppModule.kt
@@ -0,0 +1,36 @@
+package com.elhady.weather_compose.di
+
+import com.elhady.weather_compose.data.remote.WeatherApiService
+import com.elhady.weather_compose.data.remote.WeatherApiServiceImpl
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+import org.koin.dsl.module
+
+val appModule = module {
+
+    single {
+        HttpClient(Android) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+            install(Logging) {
+                level = LogLevel.ALL
+            }
+        }
+    }
+
+    single<WeatherApiService> {
+        WeatherApiServiceImpl(get())
+    }
+
+}
