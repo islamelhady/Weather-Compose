cf9c156480105578758341031e6cf85d863f1e54
islam elhady <i.elhady44@gmail.com>
2025-06-21 10:41:49 +0300

    feat(location): Integrate location tracking for weather data - add LocationTracker interface and its implementation using FusedLocationProviderClient - add location permissions to AndroidManifest - update Koin module to provide location services - modify WeatherViewModel to use LocationTracker to fetch weather for the user's current location

--- app/src/main/java/com/elhady/weather_compose/di/AppModule.kt
+++ app/src/main/java/com/elhady/weather_compose/di/AppModule.kt
@@ -1,10 +1,13 @@
 package com.elhady.weather_compose.di
 
+import com.elhady.weather_compose.data.location.LocationTrackerImp
 import com.elhady.weather_compose.data.remote.WeatherApiService
 import com.elhady.weather_compose.data.remote.WeatherApiServiceImpl
 import com.elhady.weather_compose.data.repository.WeatherRepositoryImp
+import com.elhady.weather_compose.domain.location.LocationTracker
 import com.elhady.weather_compose.domain.repository.WeatherRepository
 import com.elhady.weather_compose.presentation.WeatherViewModel
+import com.google.android.gms.location.LocationServices
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -12,6 +15,7 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -40,7 +44,16 @@ val appModule = module {
         WeatherRepositoryImp(get())
     }
     viewModel() {
-        WeatherViewModel(get())
+        WeatherViewModel(get(), get())
+    }
+    single {
+        LocationServices.getFusedLocationProviderClient(androidApplication())
+    }
+    single<LocationTracker> {
+        LocationTrackerImp(
+            locationClient = get(),
+            application = androidApplication()
+        )
     }
 
 }
